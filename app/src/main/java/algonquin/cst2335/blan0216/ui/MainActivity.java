dec72c9c3f666caefabf80c08a0de517b8c4bb3b
Hannah Hazelwood <hannahkhazelwood@gmail.com>
2023-09-23 22:18:49 -0400

    Week 2 complete

--- app/src/main/java/algonquin/cst2335/blan0216/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/blan0216/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     Switch mySwitch;
     CheckBox myCheck;
     String toggleToast;
+    String imageToast;
+    ImageButton myImageButton;
     private ActivityMainBinding varBinding;
 
     @Override
@@ -48,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
         this.myCheck = findViewById(R.id.checkBox);
         this.mySwitch = findViewById(R.id.switch1);
         this.toggleToast = "";
+        this.myImageButton = findViewById(R.id.imageButton);
+        this.imageToast = "";
 
         myText.setText(String.format("Your edit text has: %s", editString));
 
@@ -69,12 +74,27 @@ public class MainActivity extends AppCompatActivity {
             varBinding.radioButton.setChecked(selected);
             varBinding.switch1.setChecked(selected);
             toggleToast = String.format("The value is now %s", selected.toString());
-            Toast.makeText(this, toggleToast, Toast.LENGTH_SHORT).show();
+            toggledToast();
         });
 
         myRadio.setOnCheckedChangeListener( (btn, isChecked)->{ model.checkBool.postValue(isChecked); });
         myCheck.setOnCheckedChangeListener((btn, isChecked)->{ model.checkBool.postValue(isChecked); });
         mySwitch.setOnCheckedChangeListener((btn, isChecked)->{ model.checkBool.postValue(isChecked); });
 
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageToast = String.format("width = %d, height = %d", varBinding.imageButton.getWidth(), varBinding.imageButton.getHeight());
+                toastButton();
+            }
+        });
+    }
+
+    public void toastButton(){
+        Toast.makeText(this, imageToast, Toast.LENGTH_SHORT).show();
+    }
+
+    public void toggledToast(){
+        Toast.makeText(this, toggleToast, Toast.LENGTH_SHORT).show();
     }
 }
